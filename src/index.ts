bf7ff431edb1ef815ced91e5c0cd3f0786c47a24
Andrew Dibble <andrew.dibble@doctolib.com>
2020-06-12 09:45:34 +0200

    Add null import example

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,16 @@
-const greet = (): void => {
-  console.log('Hello world');
+import { Example } from './types';
+
+import Enumber = Example.Enumber;
+
+const detectNumber = (number: Enumber): void => {
+  if (number in Enumber) {
+    console.log(Enumber[number]);
+  } else {
+    console.log("I don't know that number");
+  }
 };
 
-greet();
+detectNumber(0);
+detectNumber(1);
+detectNumber(2);
+detectNumber(3);
---
+++ src/types.ts
@@ -0,0 +1,8 @@
+// eslint-disable-next-line @typescript-eslint/no-namespace
+export namespace Example {
+  export enum Enumber {
+    Zero,
+    One,
+    Two,
+  }
+}
